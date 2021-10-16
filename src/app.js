96e0242ac94c7527eafa323c12ec46a68070c0e7
abellasjuan <juanvictoraa@gmail.com>
2021-10-16 12:55:34 -0300

    back categories done

--- src/app.js
+++ src/app.js
@@ -17,9 +17,38 @@ port: 5432,
 database: 'boardcamp'
 });
 
-app.get("/categories", async (req, res) => {
+//GET CATEGORIES
+app.get("/categories", async (req, resp) => {
     const result = await connection.query(`SELECT * FROM categories`);
-    res.send(result.rows);
+    //coloquei dentro da const result o 'await' para travar ate ele responder e fiz o connection.query para conectar com o banco de dados o pedido de selecionar todas as categories;
+    resp.send(result.rows);
+    //a resposta que me é enviada sao todas as fileiras da minha requisiçao;
+});
+
+//POST CATEGORIES
+app.post('/categories', async (req, resp) => {
+    const name = req.body.name;
+    //aqui eu peguei o que tinha dentro do body. Só tinha o 'name';
+
+    if(!name){
+        //se !name retornar true quer dizer que nao foi enviado nada e dá erro;
+        return resp.sendStatus(400);
+        //me responde status 400;
+    }
+
+    const allCategoriesName = await connection.query(`SELECT * FROM categories`);
+    //coloquei na variavel as categorias existentes
+    if(allCategoriesName.rows.some(category => category.name === name)){
+        //peguei a array das categorias e percorri com SOME pra me retornar true or false se já existir alguma categoria igual;
+        return resp.sendStatus(409);
+        //se tiver aquele nome vai me retornar status 409;;
+    }
+
+    await connection.query(`INSERT INTO categories (name) VALUES ($1)`, [name]);
+    //await pra só continuar quando isso for resolvido.
+    //aqui falei pro meu banco de dados que eu quero inserir em categories no campo NAME (pq id tá automatico) e o valor que eu quero botar ('$1' dizendo que vai entrar algo ali, para evitar o sql injection) e depois passamos realmente o valor do $1, [name];
+    resp.sendStatus(201);
+    //envia status 201 se criou e deu bom;
 });
 
 app.listen(4000 , () => {
